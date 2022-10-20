a25f3a25fff21d7f49267959dcada0542612b865
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-20 22:04:53 +0000

    Finished product

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
  * Created on: Oct 2022
  * This file contains the JavaScript for index.html
  */
-function myButtonClicked() {
-  document.getElementById("address").innerHTML
+"use strict"
+/* FUnction */
+function calculateArea() {
+  /* Input */
+  const length = parseFloat(document.getElementById("lengthOfParallelogram").value)
+  const width = parseFloat(document.getElementById("widthOfParallelogram").value)
+  /* Proccess */
+  const area = length * width 
+  /* Output */
+  document.getElementById("area").innerHTML = "The area of your parallelogram is: " + area + " cm"
 }
